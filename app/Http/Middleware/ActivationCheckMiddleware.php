d2f1ae2ca61ff51218996e1f2219943b8b4d0178
m1s3rys1gn4l <m1s3rys1gn4l@gmail.com>
2026-08-26 18:22:23 +0600

    activation_control

--- app/Http/Middleware/ActivationCheckMiddleware.php
+++ app/Http/Middleware/ActivationCheckMiddleware.php
@@ -20,18 +20,18 @@ class ActivationCheckMiddleware
      * @return mixed
      */
     public function handle(Request $request, Closure $next, $area = null): mixed
-    {
-        $response = $this->checkActivationCache(app: $area);
-        if (!$response) {
-            if (!strpos(url()->current(), '/api/v1')) {
-                return Redirect::away(route(base64_decode('c3lzdGVtLmFjdGl2YXRpb24tY2hlY2s=')))->send();
-            }
+    // {
+    //     $response = $this->checkActivationCache(app: $area);
+    //     if (!$response) {
+    //         if (!strpos(url()->current(), '/api/v1')) {
+    //             return Redirect::away(route(base64_decode('c3lzdGVtLmFjdGl2YXRpb24tY2hlY2s=')))->send();
+    //         }
 
-            return response()->json([
-                'code' => 503,
-                'message' => 'Please check activation for '. str_replace('_', ' ', $area),
-            ], 503);
-        }
+    //         return response()->json([
+    //             'code' => 503,
+    //             'message' => 'Please check activation for '. str_replace('_', ' ', $area),
+    //         ], 503);
+    //     }
         return $next($request);
     }
 }
